d5ef3fda7fb25f4cdbed117ae16bf08d540790f7
Vadim Dulinets <dulinets.vadim@gmail.com>
2025-05-06 19:08:00 +0300

    log in screen layout

---
+++ app/src/main/java/com/example/chatroom/LogInFragment.kt
@@ -0,0 +1,37 @@
+package com.example.chatroom
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.chatroom.databinding.FragmentLogInBinding
+
+class LogInFragment : Fragment() {
+
+    private var _binding: FragmentLogInBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLogInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
