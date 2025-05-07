3c5cdbbebd88386155ad61de073ccadc904e8302
Vadim Dulinets <dulinets.vadim@gmail.com>
2025-05-07 22:13:00 +0300

    navigation between "log in" and "sign out"

--- app/src/main/java/com/example/chatroom/LogInFragment.kt
+++ app/src/main/java/com/example/chatroom/LogInFragment.kt
@@ -10,6 +10,7 @@ import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.chatroom.databinding.FragmentLogInBinding
 
 class LogInFragment : Fragment() {
@@ -28,10 +29,18 @@ class LogInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setUpViews()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setUpViews(){
+        binding.logInScreenSignUpButton.setOnClickListener {
+            findNavController().navigate(R.id.action_logInFragment_to_signUpFragment)
+        }
     }
 }
